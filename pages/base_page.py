c1493300096aefb81420026a8757b71ef525de47
nordzee <konstantin.popovich@telleqt.ai>
2025-01-08 02:11:52 +0400

    locators as external variable

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     # добавляем конструктор-метод
     # внутри конструктора сохраняем экземпляр драйвера и url адрес как аттрибуты нашего класса
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
