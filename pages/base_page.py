53c9fadbf799cf9ff33de43026d8bcb8405b7831
nordzee <konstantin.popovich@telleqt.ai>
2025-01-08 00:58:18 +0400

    check login link, add error processing

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     # добавляем конструктор-метод
     # внутри конструктора сохраняем экземпляр драйвера и url адрес как аттрибуты нашего класса
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     
     # метод для открывания страницы в браузере
     def open(self):
         self.browser.get(self.url)
+
+    # вспомогательный метод поиска элемента с обработкой ошибки 
+    def is_element_present(self, selector_type, selector_value):
+        try:
+            self.browser.find_element(selector_type, selector_value)
+        except NoSuchElementException:
+            return False
+        return True
